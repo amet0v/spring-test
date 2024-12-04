4455c6cb66954494916af4690e408bd6243eb152
Ametov <ametov1st@gmail.com>
2024-12-04 17:15:29 +0600

    Added GET "api/v1/book"

--- src/main/java/com/ametov/spring/book/controller/BookApiController.java
+++ src/main/java/com/ametov/spring/book/controller/BookApiController.java
@@ -1,14 +1,27 @@
 package com.ametov.spring.book.controller;
 
+import com.ametov.spring.book.entity.BookEntity;
+import com.ametov.spring.book.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class BookApiController {
+    private final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/")
     public String ok(){
         return "ok";
     }
 
+    @GetMapping("/api/v1/book")
+    public List<BookEntity> all(){
+        return BookService.all();
+    }
 }
---
+++ src/main/java/com/ametov/spring/book/service/BookService.java
@@ -0,0 +1,37 @@
+package com.ametov.spring.book.service;
+
+import com.ametov.spring.book.entity.BookEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BookService {
+    static List<BookEntity> bookStorage = new ArrayList<>();
+
+    public BookService() {
+        fillStorage();
+    }
+
+    public void fillStorage() {
+        String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus magna ac mauris malesuada, posuere efficitur nulla volutpat. Mauris laoreet a dolor sit amet iaculis. Ut consequat tellus ac orci sodales aliquet. In hac habitasse platea dictumst. Curabitur leo nisl, vestibulum quis sagittis eu, rutrum quis orci. Ut sit amet quam venenatis, efficitur dolor sit amet, tincidunt enim. Nunc ut odio arcu. Fusce ultrices velit eu eros vestibulum varius.";
+
+        for (int i = 0; i < 100; i++) {
+            Random random = new Random();
+            bookStorage.add(new BookEntity(i+1,
+                    "Book" + random.nextInt(100, 999),
+                    desc.substring(0, random.nextInt(0, desc.length()))));
+        }
+    }
+
+    public static List<BookEntity> all() {
+        return bookStorage;
+    }
+
+//    public static Optional<BookEntity> byId(Integer id){
+//        return bookStorage.stream().filter((bookEntity -> bookEntity.getId().equals(id))).findFirst();
+//    }
+}
