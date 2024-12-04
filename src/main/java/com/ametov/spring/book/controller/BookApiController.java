c18ba0c20d81b8231c06a5e0e7aa811843cc41e6
Ametov <ametov1st@gmail.com>
2024-12-04 15:04:37 +0600

    Add GET mapping

---
+++ src/main/java/com/ametov/spring/book/controller/BookApiController.java
@@ -0,0 +1,14 @@
+package com.ametov.spring.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookApiController {
+
+    @GetMapping("/")
+    public String ok(){
+        return "ok";
+    }
+
+}
